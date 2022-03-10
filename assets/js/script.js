9b3286df09e9ef0aacf19299b44d08504754e3ef
falovic <88166353+falovic@users.noreply.github.com>
2022-03-10 22:13:15 +0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,6 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
